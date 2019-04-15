1d5821843fffb5187119c98756ffb6c3a9a3236f
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2019-04-15 19:05:53 +0900

    トップページに Map へのリンクを追加

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,18 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/map">Map</router-link>
     </div>
     <router-view />
   </div>
 </template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
 
+@Component
+export default class App extends Vue {}
+</script>
 <style lang="scss">
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
